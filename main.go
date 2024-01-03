db3b3e3c21ef6b429b6647455f1c7f643ba39d66
Stephen Rhoton <stephen.rhoton@dealerware.com>
2024-01-03 09:53:48 -0500

    Make the exit code random to test failure conditions in the pipeline.

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+  "math/rand"
+)
 
 func main() {
-    fmt.Println("hello world")
+  fmt.Println("hello world")
+  xc := rand.Intn(100)
+  if xc%2 == 0 { 
+    xc = 100
+  } else {
+    xc = 0
+  }
+  fmt.Println("exiting with code: ", xc)
+  os.Exit(xc)
 }
